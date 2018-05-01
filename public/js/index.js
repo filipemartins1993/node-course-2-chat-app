4c1251448361d1fb74a06d10b37ae2b7748abd19
Filipe <filipemartins1993@gmail.com>
2018-05-01 16:21:03 +0100

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function() {
   console.log('Connected to server');
-
-  socket.emit('createMessage', {
-    to: 'jen',
-    text: 'Hey. This is Jen.'
-  });
 });
 
 socket.on('disconnect', function() {
